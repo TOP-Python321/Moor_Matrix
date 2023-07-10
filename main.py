4893a49cf9daa43453054446e52baba286fccf00
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-07-10 18:28:06 +0400

    добавление класса Matrix, конструктора и метода __str__

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+class Matrix:
+    def __init__(self, matrix: tuple[tuple[int, int, ...], ...]):
+        self.matrix = matrix
+
+    def __str__(self):
+        return '\n'.join('\t'.join(map(str, row)) for row in self.matrix)
 
