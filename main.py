d192e6956c0fc7c4cfe6c865e3c938531b9257b2
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-07-11 15:34:04 +0400

    добавление метода __add__

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ class Matrix:
     def __getitem__(self, item):
         return self.matrix[item]
 
+
+    def __add__(self, other):
+        other = Matrix(other)
+        nex_matrix = []
+        numbers = []
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                summa = other[i][j] + self.matrix[i][j]
+                numbers.append(summa)
+                if len(numbers) == len(self.matrix[0]):
+                    nex_matrix.append(numbers)
+                    numbers = []
+        return Matrix(nex_matrix)
+
+
+
+a = Matrix(((1, 2, 3), (4, 5, 6)))
+b = Matrix(((4, 7, 12), (56, 3, 2)))
+
+print(a, b, sep='\n\n')
+print(b + a)
