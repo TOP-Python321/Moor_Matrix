759637ba4b2e8f93f8515db5d2b210a4cbbe9c10
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-07-11 15:33:34 +0400

    добавление метода __getitem__

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 class Matrix:
     def __init__(self, matrix: tuple[tuple[int, int, ...], ...]):
         self.matrix = matrix
+        # self.size = (len(self.matrix), len(self.matrix[0]))
 
     def __str__(self):
         return '\n'.join('\t'.join(map(str, row)) for row in self.matrix)
+
+    def __getitem__(self, item):
+        return self.matrix[item]
 
